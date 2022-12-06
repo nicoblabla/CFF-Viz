8d893e5c62b6d7d5441c5e32cb2985d89c0b8781
Nicolas <Nicolas.Laoun@he-arc.ch>
2022-12-06 23:37:38 +0100

    FPS is now 24 time higher.

--- web/js/trains.js
+++ web/js/trains.js
@@ -2,7 +2,10 @@ function Trains() {
     let simulationSpeed = 60;
     let pause = true;
     let map;
-    this.initMap = async function() {
+    let overlay;
+    let ctx;
+    let t = 6 * 3600;
+    this.initMap = async function () {
         map = new google.maps.Map(document.getElementById("map2"), {
             zoom: 8.5,
             center: { lat: 46.773357, lng: 8.2143418 },
@@ -10,13 +13,140 @@ function Trains() {
             mapId: "634d8c221b2996df",
             gestureHandling: 'greedy'
         });
+
+        const bounds = new google.maps.LatLngBounds(
+            new google.maps.LatLng(45.7769477403, 6.02260949059),
+            new google.maps.LatLng(47.8308275417, 10.4427014502)
+        );
+        // The photograph is courtesy of the U.S. Geological Survey.
+        let image = "https://developers.google.com/maps/documentation/javascript/";
+
+        image += "examples/full/images/talkeetna.png";
+
+        class Overlay extends google.maps.OverlayView {
+            bounds;
+            canvas;
+            div;
+            overlayProjection;
+            canvas;
+            constructor(bounds, image) {
+                super();
+                this.bounds = bounds;
+                this.image = image;
+            }
+
+            onAdd() {
+                this.div = document.createElement("div");
+                this.div.style.borderStyle = "none";
+                this.div.style.borderWidth = "0px";
+                this.div.style.position = "absolute";
+
+                // Create the img element and attach it to the div.
+                /*const img = document.createElement("img");
+
+                img.src = this.image;
+                img.style.width = "100%";
+                img.style.height = "100%";
+                img.style.position = "absolute";
+                this.div.appendChild(img);*/
+                this.canvas = document.createElement('canvas');
+                this.canvas.width = "100%";
+                this.canvas.height = "100%";
+                ctx = this.canvas.getContext("2d");
+                ctx.fillStyle = '#FF0000';
+                ctx.fillRect(10, 10, 100, 100);
+                this.div.appendChild(this.canvas);
+
+                // Add the element to the "overlayLayer" pane.
+                const panes = this.getPanes();
+
+                panes.overlayLayer.appendChild(this.div);
+            }
+            draw() {
+                console.log("draw1");
+                // We use the south-west and north-east
+                // coordinates of the overlay to peg it to the correct position and size.
+                // To do this, we need to retrieve the projection from the overlay.
+                this.overlayProjection = this.getProjection();
+                // Retrieve the south-west and north-east coordinates of this overlay
+                // in LatLngs and convert them to pixel coordinates.
+                // We'll use these coordinates to resize the div.
+                const sw = this.overlayProjection.fromLatLngToDivPixel(
+                    this.bounds.getSouthWest()
+                );
+                const ne = this.overlayProjection.fromLatLngToDivPixel(
+                    this.bounds.getNorthEast()
+                );
+
+                // Resize the image's div to fit the indicated dimensions.
+                if (this.div) {
+                    this.div.style.left = sw.x + "px";
+                    this.div.style.top = ne.y + "px";
+                    this.div.style.width = ne.x - sw.x + "px";
+                    this.div.style.height = sw.y - ne.y + "px";
+                    if (this.canvas.width != ne.x - sw.x) {
+                        this.canvas.width = ne.x - sw.x;
+                        this.canvas.height = sw.y - ne.y;
+                    }
+                }
+
+
+                
+
+            }
+
+            refresh(t) {
+
+
+                if (!pause) {
+                    setTimeout(() => {
+                        if (!pause) {
+                            currentSeconds += simulationSpeed;
+                            currentSeconds %= 86400; // 24 * 3600
+                            this.refresh(currentSeconds)
+                        }
+                    }, 60);
+                }
+
+                if (!this.overlayProjection)
+                return;
+                console.log("refresh1");
+                const sw = this.overlayProjection.fromLatLngToDivPixel(
+                    this.bounds.getSouthWest()
+                );
+                const ne = this.overlayProjection.fromLatLngToDivPixel(
+                    this.bounds.getNorthEast()
+                );
+
+                let trains = getTrains(t);
+                ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+                ctx.fillStyle = "red";
+                document.getElementById('clock').innerHTML = secondToTime(t);
+                for (let i = 0; i < trains.length; i++) {
+
+                    const pos = this.overlayProjection.fromLatLngToDivPixel(
+                        { lat: trains[i][0], lng: trains[i][1] }
+                    );
+
+                    ctx.beginPath();
+                    console.log(pos.x-sw.x, pos.y-ne.y);
+                    ctx.arc(pos.x-sw.x, pos.y-ne.y, 3, 0, 2 * Math.PI);
+                    ctx.fill();
+                }
+            }
+
+        }
+
+        overlay = new Overlay(bounds, image);
+
+        overlay.setMap(map);
+
     }
 
     let currentSeconds = null;
     let stopTimes = null;
     let stops = null;
 
-    let t = 6 * 3600;
     let circles = [];
     function draw(t) {
         console.log("draw");
@@ -44,7 +174,7 @@ function Trains() {
             circles[i].setMap(null);
         }
 
-        if (!pause) {
+        /*if (!pause) {
             setTimeout(() => {
                 if (!pause) {
                     currentSeconds += simulationSpeed;
@@ -52,19 +182,19 @@ function Trains() {
                     draw(currentSeconds)
                 }
             }, 1);
-        }
+        }*/
     }
 
     function getTrains(t) {
         let results = [];
         for (let i = 0; i < stopTimes.length; i++) {
-            if (t >= stopTimes[i][2]+15 && t <= stopTimes[i][3]) { // Trains in station
+            if (t >= stopTimes[i][2] + 15 && t <= stopTimes[i][3]) { // Trains in station
                 results.push(stops[stopTimes[i][1]]);
             }
-            if (stopTimes[i][0] == stopTimes[i+1]?.[0] && t > stopTimes[i][3] && t < stopTimes[i+1][2]+15) { // Trains between i and i+1
+            if (stopTimes[i][0] == stopTimes[i + 1]?.[0] && t > stopTimes[i][3] && t < stopTimes[i + 1][2] + 15) { // Trains between i and i+1
                 let p1 = stops[stopTimes[i][1]];
-                let p2 = stops[stopTimes[i+1][1]];
-                let progression = (t - stopTimes[i][3]) / ((stopTimes[i+1][2]+15) - stopTimes[i][3])
+                let p2 = stops[stopTimes[i + 1][1]];
+                let progression = (t - stopTimes[i][3]) / ((stopTimes[i + 1][2] + 15) - stopTimes[i][3])
                 results.push([
                     p1[0] + progression * (p2[0] - p1[0]),
                     p1[1] + progression * (p2[1] - p1[1])
@@ -105,7 +235,7 @@ function Trains() {
         return Math.round(v * e) / e;
     }
 
-    this.start = async function() {
+    this.start = async function () {
 
         if (!stopTimes) {
             let buffer = await (await fetch('../data/stop_times.json.lzs')).arrayBuffer();
@@ -115,14 +245,15 @@ function Trains() {
         }
 
         pause = false;
-        draw(currentSeconds)
+        //draw(currentSeconds)
+        overlay.refresh(currentSeconds);
     }
 
-    this.stop = function() {
+    this.stop = function () {
         pause = true;
     }
 
-    this.changeSimulationSpeed = function(speed) {
+    this.changeSimulationSpeed = function (speed) {
         simulationSpeed = speed;
     }
 }
